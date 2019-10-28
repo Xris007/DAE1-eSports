6bfb274a10b8855f0a80cf80db6150214991b13b
croudebush15 <50419898+croudebush15@users.noreply.github.com>
2019-10-28 15:59:28 -0500

    convertir en web

--- DAE1-project/src/main/java/pe/isil/model/DetalleEquipo.java
+++ DAE1-project/src/main/java/pe/isil/model/DetalleEquipo.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 
 public class DetalleEquipo {
     private Integer IdEquipo;
@@ -14,4 +11,55 @@ public class DetalleEquipo {
     private Integer Victorias;
     private Integer Derrotas;
     private Integer Puesto;
+
+    public DetalleEquipo() {
+    }
+
+    public Integer getIdEquipo() {
+        return IdEquipo;
+    }
+
+    public DetalleEquipo(Integer idEquipo, Integer idTorneo, Integer victorias, Integer derrotas, Integer puesto) {
+        IdEquipo = idEquipo;
+        IdTorneo = idTorneo;
+        Victorias = victorias;
+        Derrotas = derrotas;
+        Puesto = puesto;
+    }
+
+    public void setIdEquipo(Integer idEquipo) {
+        IdEquipo = idEquipo;
+    }
+
+    public Integer getIdTorneo() {
+        return IdTorneo;
+    }
+
+    public void setIdTorneo(Integer idTorneo) {
+        IdTorneo = idTorneo;
+    }
+
+    public Integer getVictorias() {
+        return Victorias;
+    }
+
+    public void setVictorias(Integer victorias) {
+        Victorias = victorias;
+    }
+
+    public Integer getDerrotas() {
+        return Derrotas;
+    }
+
+    public void setDerrotas(Integer derrotas) {
+        Derrotas = derrotas;
+    }
+
+    public Integer getPuesto() {
+        return Puesto;
+    }
+
+    public void setPuesto(Integer puesto) {
+        Puesto = puesto;
+    }
 }
