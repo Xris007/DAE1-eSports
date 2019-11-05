397fd3d6f75f2329fb68bed3e9066f488f523e35
Your Name <you@example.com>
2019-11-04 22:12:11 -0500

    persistence change

--- DAE1-project/src/main/java/pe/isil/Main.java
+++ DAE1-project/src/main/java/pe/isil/Main.java
@@ -1,10 +1,12 @@
 package pe.isil;
 
 import pe.isil.model.Usuario;
+import pe.isil.service.UsuarioService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Main {
 
@@ -12,22 +14,26 @@ public class Main {
     private static EntityManagerFactory builder;
 
     public static void main(String[] args) {
-        builder = Persistence.createEntityManagerFactory("isilPU");
+        builder = Persistence.createEntityManagerFactory("eSportsPU");
         entityManager = builder.createEntityManager();
 
         entityManager.getTransaction().begin();
-        entityManager.getTransaction().rollback();
-        entityManager.getTransaction().commit();
+        //entityManager.getTransaction().rollback();
+        //entityManager.getTransaction().commit();
 
         Usuario usuario = new Usuario();
-        usuario.setUserid(1000);
+        usuario.setUserid(2000);
         usuario.setUsername("DoctorSasquatch");
         usuario.setPassword("1234");
 
-        entityManager.persist(usuario);
-        Usuario usuario1 = entityManager.find(Usuario.class, 1000);
-        System.out.println("User = " + usuario1);
+        UsuarioService usuarioService = new UsuarioService();
 
-        entityManager.getTransaction().commit();
+        usuarioService.create(usuario);
+        List<Usuario> usuarios = usuarioService.findAll();
+        System.out.println("Lista de Usuarios: " + usuarios);
+
+
+//        usuarioService.delete(2000);
+
     }
 }
